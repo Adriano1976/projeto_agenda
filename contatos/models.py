056926ffab9dc83198b4d45c7b7c50e251bdcef3
Adriano Santos <adriradiologista@hotmail.com.br>
2022-04-07 15:48:34 -0300

    Update settings.py

--- contatos/models.py
+++ contatos/models.py
@@ -11,14 +11,14 @@ class Categoria(models.Model):
 
 class Contato(models.Model):
     objects = None
-    nome = models.CharField(max_length=255)
-    sobrenome = models.CharField(max_length=255, blank=True)
-    telefone = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, blank=True)
-    data_criacao = models.DateTimeField(default=timezone.now)
-    descricao = models.TextField(blank=True)
+    nome = models.CharField(max_length=255, verbose_name='nome')
+    sobrenome = models.CharField(max_length=255, blank=True, verbose_name='sobrenome')
+    telefone = models.CharField(max_length=255, verbose_name='telefone')
+    email = models.CharField(max_length=255, blank=True, verbose_name='e-mail')
+    data_criacao = models.DateTimeField(default=timezone.now, verbose_name='data de criação')
+    descricao = models.TextField(blank=True, verbose_name='descrição')
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
-    mostrar = models.BooleanField(default=True)
+    mostrar = models.BooleanField(default=True, verbose_name='categoria')
     foto = models.ImageField(blank=True, upload_to='fotos/%Y/%m/%d')
 
     def __str__(self):
