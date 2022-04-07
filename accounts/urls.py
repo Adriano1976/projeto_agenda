e817af4e1f7199cf19987e06322ab1ed4fab1841
Adriano Santos <adriradiologista@hotmail.com.br>
2022-04-07 15:03:33 -0300

    Update file _nav e base.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,11 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='index_login'),
+    # /accounts/index_login
     path('login/', views.login, name='login'),
+    # /accounts/login/
     path('logout/', views.logout, name='logout'),
+    # /accounts/logout/
     path('register/', views.register, name='register'),
+    # /accounts/register/
     path('dashboard/', views.dashboard, name='dashboard'),
+    # /accounts/dashboard/
+
     # ATENçÃO: --- ↑ - Não esquecer da barra!!!
-    # Lembre-se que no: djAgPy\agendaContatos\urls.py
+    # Lembre-se que no: djAgPy\agenda\Contatos\urls.py
     # Também deve ter a barra no final.
 ]
